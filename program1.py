ae8f8cc615daf240be562a82ce11730a8c1b3c73
MohammadtAsari <118717928+MohammadtAsari@users.noreply.github.com>
2024-02-23 13:45:04 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -4,10 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        bracket_pairs = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in bracket_pairs.values():
+                stack.append(char)
+            elif char in bracket_pairs.keys():
+                if not stack or stack.pop() != bracket_pairs[char]:
+                    return False
+            else:
+                return False
+
+        return not stack
     
-
-
+    pass
 
   
 
